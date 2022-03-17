3133385dff90890f0c0cc564eec6c20766478c48
Gabrielish <gabriel_bundea@hotmail.com>
2022-03-17 22:44:17 +0200

    sitekey

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -77,7 +77,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6LcAu-IdAAAAAJOTI5E_eRltZNQCvukIl2-f1glQ"
+          sitekey="6Ld2xOoeAAAAAPYVAAR7iy3pdKqIwTnb1ybnyuj3"
           onChange={(e) => {
             setIsHuman(true)
           }}
