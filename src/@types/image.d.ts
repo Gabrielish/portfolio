0cb7db87b74a978b25d4f52fbad245543d2b5ce1
Gabrielish <gabriel_bundea@hotmail.com>
2022-03-24 21:50:45 +0200

    Update

--- src/@types/image.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.svg';
-declare module '*.pdf';
-declare module '*.png';
-declare module '*.jpg';
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { useEffect } from 'react'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { GlobalStyle } from './styles/global'
 import 'react-toastify/dist/ReactToastify.css'
+
 function App() {
   return (
     <>
@@ -13,7 +13,7 @@ function App() {
       <Main></Main>
       <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
