661e0a2e297bd4167c90e93d0c68027dfbc069fd
Gabrielish <gabriel_bundea@hotmail.com>
2022-05-08 13:52:04 +0300

    changes2

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -16,14 +16,14 @@ export function Portfolio(){
           <header>
           <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#F44250" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
           <div className="project-links">
-            <a href="https://github.com/Gabrielish" target="_blank" rel="noreferrer">
-              <img src={githubIcon} alt="GitHub" />
+            <a href="https://gabrielbundea.imgbb.com/albums" target="_blank" rel="noreferrer">
+              <img src={externalLinkIcon} alt="GitHub" />
             </a>
           </div>
           </header>
           <div className="body">
             <h3>Graphic Designs</h3>
-            <p>Here are all my projects within the<a href="https://agropro.ro/"> company</a> I work in for 2 years, from banners, product designs, product labels, box designs made in adobe photoshop / illustrator.
+            <p>Here are all my projects within the<a href="https://agropro.ro/"> company</a> I work in for 2 years, from banners, product designs, product labels, box designs.
             </p>
           </div>
           <footer>
